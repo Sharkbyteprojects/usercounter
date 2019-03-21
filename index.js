bce4c709c453198ff5ef0953e21fe84f9f56cec6
Sharkbyteprojects <sharkbyteprojects@protonmail.com>
2019-03-21 17:10:12 +0100

    input counter

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ const express = require('express');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const app = express();
+let connectionslst = [
+    {connections: 0}
+];
+var connections = 0;
+app.engine('handlebars', exphbs());
+app.get('/count', (request, response) => {
+    connectionslst[1].connections + 1;
+    connections++;
+    });
+app.set('view engine', 'handlebars');
 app.use(morgan('common', { immediate: true }));
+app.get('/', (request, response) => {
+const connectionslistener = connections;
+response.render(__dirname + '/transfer.handlebars', {connections: connectionslistener});
+});
 app.use(express.static(__dirname + '/content'));
 app.listen(80, () => {
     console.log("SERVER ONLINE ON http://localhost/");
