48da1fd29b61791887c3c394854e6303c23feba1
Sharkbyteprojects <sharkbyteprojects@protonmail.com>
2019-03-21 16:49:39 +0100

    all

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const app = express();
 app.use(morgan('common', { immediate: true }));
+app.use(express.static(__dirname + '/content'));
 app.listen(80, () => {
-    console.log("SERVER ONLINE ON http://localhost");
+    console.log("SERVER ONLINE ON http://localhost/");
 });
