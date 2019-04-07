59e84b48a4888c21cb72ee56c03384bfa5f3893d
Sharkbyteprojects <sharkbyteprojects@protonmail.com>
2019-04-07 16:27:41 +0200

    -

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const exphbs = require('express-handlebars');
 const app = express();
-const connectionsa = require('easy-array_sytem');
-connectionsa.generate({conn: 0});
 var connections = 0;
 const jsonraw = [
   { name: 'github', url: 'https://github.com/Sharkbyteprojects/usercounter' },
@@ -16,7 +14,6 @@ app.use(helmet.xssFilter());
 app.engine('handlebars', exphbs());
 app.get('/count', (request, response) => {
     connections++;
-	connectionsa.changee("0", "conn", eval(`"${connections}"`));
     response.redirect('/');
     });
 app.get('/repos', (request, response) => {
@@ -26,17 +23,8 @@ app.get('/raw', (request, response) => {
     var stringbuild = "" + connections + "";
     response.send(stringbuild);
 });
-app.get('/rawjson', (request, response) => {
-    response.send(connectionsa.readall());
-});
-app.get('/rawjson/count', (request, response) => {
-	connections++;
-	connectionsa.changee("0", "conn", eval(`"${connections}"`));
-    response.redirect('/rawjson');
-});
 app.get('/raw/count', (request, response) => {
     connections++;
-	connectionsa.changee("0", "conn", eval(`"${connections}"`));
     response.redirect('/raw');});
 app.set('view engine', 'handlebars');
 app.use(morgan('common', { immediate: true }));
